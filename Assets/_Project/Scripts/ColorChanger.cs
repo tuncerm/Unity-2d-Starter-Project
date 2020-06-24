1fbac97dc0e4ce877003c916c7e04c122d91d6f4
Musap Kahraman <musapkahraman@gmail.com>
2020-06-24 12:46:12 +0300

    Add a color changer on sprite shape click

---
+++ Assets/_Project/Scripts/ColorChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace KoltukStudio
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class ColorChanger : MonoBehaviour
+    {
+        private SpriteRenderer _renderer;
+
+        private void Start()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void OnMouseDown()
+        {
+            Debug.Log(name + " is clicked!");
+            var r = Random.Range(0f, 1f);
+            var g = Random.Range(0f, 1f);
+            var b = Random.Range(0f, 1f);
+            Debug.Log($"New color values are r: {r}, g: {g}, b: {b}");
+            _renderer.color = new Color(r,g,b);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Platforms       : Windows, Android
 Editor          : Unity 2019.4.1f1
 Render Pipeline : Universal RP
 ```
+The command-line argument for Windows 32-bit:
+```
+-buildTarget Win
+```
 The command-line argument for Android:
 ```
 -buildTarget Android
